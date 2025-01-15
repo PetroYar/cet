da5b5342736a40aad3115df590cbe1214196d613
PetroYar <petryarish@gmail.com>
2025-01-15 22:57:40 +0200

    fff

--- script.js
+++ script.js
@@ -1,43 +1,139 @@
-function Player(name, age, position, nationality, price) {
-  this.name = name;
-  this.age = age;
-  this.position = position;
-  this.nationality = nationality;
-  this.price = price;
+const footballManager = {
+  player: [],
+  club: [],
+  transfer: [],
+};
+
+function Base() {
+  const uid = Math.floor(Math.random() * 10000);
+  return {
+    uid,
+    date: new Date().toLocaleDateString("uk-UA"),
+  };
 }
 
-function Club(name, city, stadion, budget) {
-  this.name = name;
-  this.city = city;
-  this.stadion = stadion;
-  this.budget = budget;
-  this.players = [];
-
-  this.addPlayers = function (player) {
-    this.players.push(player);
+function Player({ name, position, age, price, clubId = null }) {
+  return {
+    ...Base(),
+    name,
+    position,
+    age,
+    price,
+    clubId,
   };
+}
 
-  this.showInfo = function () {
-    console.log(`club ${this.name} \nplayers:`);
-    
-    this.players.forEach((player) => {
-      console.log(player.name);
+function Club(name, budget) {
+  return {
+    ...Base(),
+    name,
+    budget,
+  };
+}
+
+function addPlayer({ name, position, age, price, clubId }) {
+  const newPlayer = Player({ name, position, age, price, clubId });
+  footballManager.player.push(newPlayer);
+}
+function addClub(name, budget) {
+  const newClub = Club(name, budget);
+  footballManager.club.push(newClub);
+}
+
+function getInfoClub(uid) {
+  const club = footballManager.club.find((club) => club.uid === uid);
+  if (club) {
+    console.log(
+      `club name ${club.name} \nclub budget ${club.budget} \nplayers:`
+    );
+
+    footballManager.player.forEach((player) => {
+      if (player.clubId === club.uid) {
+        console.log(player.name);
+      } else {
+        console.log("not player");
+      }
     });
-  };
+  } else {
+    console.log("not clube");
+  }
+}
+function buyPlayer({ playerId, sellClubId, buyClubId }) {
+  const player = footballManager.player.find(
+    (player) => player.uid === playerId
+  );
+  const sellClub = footballManager.club.find((club) => club.uid === sellClubId);
+  const buyClub = footballManager.club.find((club) => club.uid === buyClubId);
+
+  if (player && buyClubId && sellClubId) {
+    if (buyClub.budget >= player.price) {
+      buyClub.budget -= player.price;
+      sellClub.budget += player.price;
+
+      footballManager.player = footballManager.player.map((data) => {
+        return {
+          ...data,
+          clubId: buyClubId,
+          buyDate: new Date().toLocaleDateString("uk-UA"),
+        };
+      });
+
+      const newTransfer = {
+        playerId,
+        sellClubId,
+        buyClubId,
+        price: player.price,
+        buyDate: new Date().toLocaleDateString("uk-UA"),
+      };
+
+      footballManager.transfer.push(newTransfer);
+    } else {
+      console.log("The club is free of money");
+    }
+  } else if (player && buyClub) {
+    footballManager.player = footballManager.player.map((data) => {
+      return {
+        ...data,
+        clubId: buyClubId,
+        buyDate: new Date().toLocaleDateString("uk-UA"),
+      };
+    });
+  } else {
+    console.log("error date");
+  }
 }
 
-function Transfer(player, seller, buyer, price) {
-  this.player = player;
-  this.seller = seller;
-  this.buyer = buyer;
-  this.price = price;
-}
+addClub("barsa", 33_000);
+addClub("liver", 22_000);
+addPlayer({
+  name: "petro",
+  age: 33,
+  position: "center",
+  price: 1_400,
+});
+addPlayer({
+  name: "ivan",
+  age: 24,
+  position: "defender",
+  price: 1_100,
+});
+buyPlayer({
+  buyClubId: footballManager.club[0]?.uid,
+  playerId: footballManager.player[0]?.uid,
+});
+buyPlayer({
+  buyClubId: footballManager.club[0]?.uid,
+  playerId: footballManager.player[0]?.uid,
+  sellClubId: footballManager.club[1]?.uid,
+});
+getInfoClub(footballManager.club[0]?.uid);
+// infoClub(footballManager.club[0].uid);
+console.log(footballManager);
 
-const player1 = new Player("zidan", 44, "center", "italia", 3300);
-const player2 = new Player("ivan", 44, "defender", "france", 300);
+// function sellPlayer(playerId, clubId) {
+//   const player = footballManager.player.find(
+//     (player) => player.uid === playerId
+//   );
+//   const club = footballManager.player.find((club) => club.uid === clubId);
 
-const dynamo = new Club("dynamo");
-dynamo.addPlayers(player1);
-dynamo.addPlayers(player2);
-
-dynamo.showInfo();
+// }
